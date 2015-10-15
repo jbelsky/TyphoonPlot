9963be7e298c98e94010a2657c53f5a5252062de
Jason A Belsky <jabelsky@gmail.com>
2015-10-15 13:50:29 -0400

    Updated functions for usage!

--- R/create_typhoon_plot_functions.R
+++ R/create_typhoon_plot_functions.R
@@ -1,23 +1,6 @@
-# Name of functions contained in this functions file
-# FUNCTION NAME			FUNCTION DEFINITION
-# get_typhoon_plot_mat  	function(chr, start_pos, end_pos, bam_file_name)
-# read_in_paired_bam 		function(bam_file_name, chr, type = "GR")
-# dens_dot_plot 		function(dot.m, z_min = 0, z_max = 100, ...)
-# make_gene_schematic 		function(feature_chr, feature_start, feature_end, ...)
-# plot_gene 			function(gene.v, ylow, yhigh, xstart, xend, cex_title, geneName, x_pos_title = 50){
-# set_chromatin_schematic	function(x_start = 0, x_end = 1, y_start = 0, y_end = 1){
-
-# Load the libraries
-# library(GenomicRanges)
-# library(Rsamtools)
-# library(gplots)
-
 # Ensure that the options(stringsAsFactors = FALSE)
 old_options <- options(stringsAsFactors = FALSE)
 on.exit(options(old_options), add = TRUE)
-
-# Get the process_bam_functions
-# source("/data/illumina_pipeline/scripts/process_bam_scripts/process_bam_functions.R")
 
 #' Get the TyphoonPlot matrix
 #'
@@ -32,91 +15,56 @@ on.exit(options(old_options), add = TRUE)
 GetTyphoonPlotMat = function(bam_file_name, chr, start_pos, end_pos){
 
 	# Load the bam_file
-	chr.gr = convert_paired_read_bam_to_gr(bam_file_name, chr, start_pos, end_pos)
+	chr.gr = ConvertPairedReadBAMToGR(bam_file_name, chr, start_pos, end_pos)
 
 	# Subset on reads less than 250 bp
 	chr.gr = chr.gr[width(chr.gr) <= 250]
 
 	# Set the query GR
-	query.gr = GRanges(seqnames = chr,
-			   ranges = IRanges(start = start_pos, end = end_pos)
-			  )
+	query.gr = GenomicRanges::GRanges(seqnames = chr,
+			                              ranges = IRanges::IRanges(start = start_pos, end = end_pos)
+			                             )
 
 	# Subset on the indices that overlap with the query.gr
-	idx = subjectHits(findOverlaps(query.gr, chr.gr))
+	idx = S4Vectors::subjectHits(IRanges::findOverlaps(query.gr, chr.gr))
 	chr.gr = chr.gr[idx]
 
 	# Get a new typhoon_plot_gr that will contain the positions of the "half width" reads
 	chr_tp.gr = chr.gr
 
 	# Update the start and end coordinates of the "half width" reads
-	ranges(chr_tp.gr) = IRanges(start = start(chr.gr) + round(width(chr.gr) / 4),
-				    end = end(chr.gr) - round(width(chr.gr) / 4)
-				   )
+	IRanges::ranges(chr_tp.gr) = IRanges::IRanges(start = start(chr.gr) + round(width(chr.gr) / 4),
+				                                        end = end(chr.gr) - round(width(chr.gr) / 4)
+				                                       )
 
 	# Set up the matrix
 	mat.m = matrix(0, nrow = 250, ncol = end_pos - start_pos + 1)
 	colnames(mat.m) = start_pos:end_pos
 
 	# Create the sub feature
-	mat.gr = GRanges(seqnames = chr, ranges = IRanges(start = start_pos:end_pos, width = 1))
+	mat.gr = GenomicRanges::GRanges(seqnames = chr,
+                                  ranges = IRanges::IRanges(start = start_pos:end_pos, width = 1)
+                                 )
 
 	# Iterate through each fragment width
 	for(i in 20:250){
 
 		# Get the reads that have a particular fragment width
-		idx = which(width(chr.gr) == i)
+		idx = which(BiocGenerics::width(chr.gr) == i)
 
 		if(any(idx)){
 
 			# Count the overlaps with the mat.gr
-			mat.m[i,] = countOverlaps(mat.gr, chr_tp.gr[idx])
+			mat.m[i,] = IRanges::countOverlaps(mat.gr, chr_tp.gr[idx])
 
 		}
 
 	}
 
-	# Scale the mat to 20,000,000 reads
-	mat.m = mat.m / (get_total_read_number(bam_file_name, as.character(1:16)) / 20E6)
-
 	# Return the mat.m
 	return(mat.m)
 
 }
-
-get_typhoon_plot_mat_java = function(bam_file_name, chr, start, end){
-
-	# Set the jar_file
-	jar_file = "/data/illumina_pipeline/scripts/java_scripts/jar_files/get-typhoon-plot-R.jar"
-
-	# Create the temporary file
-	temp_file = paste(".", paste(chr, start, end, sep = "_"), sep = "")
-
-	cat("The temp file is\t", temp_file, "\n")
-
-	# Create the output string
-	output_str = paste("java -jar", jar_file, bam_file_name, chr, start, end, temp_file)
-
-	# Run the command
-	system(command = output_str)
-
-	# Read in the temp_file
-	cat("Reading in\t", temp_file, "...\n")
-	mat.df = read.csv(temp_file, header = T, row.names = 1, check.names = F)
-
-	# Remove the temporary file
-	file.remove(temp_file)
-
-	# Convert into matrix
-	mat.m = as.matrix(mat.df)
-
-	# Return the mnase_dens.v
-	return(mat.m)
-
-}
-
-
-
 
 #' Makes a heatmap of data within a matrix
 #'
@@ -134,7 +82,7 @@ get_typhoon_plot_mat_java = function(bam_file_name, chr, start, end){
 #' @return A heatmap plot utilizing the image function
 #' @examples
 #' \dontrun{DensDotPlot(dot.m = typhoon.m, z_max = 50)}
-DensDotPlot = function(dot.m, z_min = 0, z_max = 100,
+DensDotPlot = function(dot.m, z_min = 0, z_max = NA,
 			 low_col = "white", med_col = "", high_col = "blue", num_colors = 100,
 		   plot_title = "", plot_title_line = NA,
        x_label = "", y_label = "",
@@ -146,10 +94,16 @@ DensDotPlot = function(dot.m, z_min = 0, z_max = 100,
     stop("dot.m is not a matrix, DensDotPlot requires a matrix!")
   }
 
+  if(is.na(z_max)){
+    # Make the z_max equivalent to the 95th percentile if z_max is not specified
+    z_max = quantile(as.vector(dot.m), probs = 0.95)
+
+  }
+
 	# For points that are either above or below z_max or z_min respectively, set them to
 	# the z_min and z_max (otherwise, plot shows arbitrary colors
   dot.m[which(dot.m >= z_max)] = z_max
-	dot.m[which(dot.m <= z_min)] = z_min
+  dot.m[which(dot.m <= z_min)] = z_min
 
 	# Get the current column names
 	if(!is.null(colnames(dot.m))){
@@ -188,8 +142,9 @@ DensDotPlot = function(dot.m, z_min = 0, z_max = 100,
 }
 
 # Set up the gene
-make_gene_schematic = function(feature_chr, feature_start, feature_end,
+MakeGeneSchematic = function(feature_chr, feature_start, feature_end,
 			       y_low = 0, y_high = 1, cex_title = 1, bg_type = "white",
+             fwd_gene_col = "gray", rev_gene_col = "gray",
 			       proteinCoding = T, geneName = T, omit_genes = NA, x_pos_title = 50,
 			       gene_file_name = "/data/illumina_pipeline/scripts/feature_files/yeast/gene_tables/sacCer2_ucsc_sgdGeneTable.csv"
 			      ){
@@ -221,26 +176,31 @@ make_gene_schematic = function(feature_chr, feature_start, feature_end,
 	}
 
 	# Convert to a GenomicRanges object
-	gene.gr = GRanges(seqnames = gene.df$chr,
+	gene.gr = GenomicRanges::GRanges(seqnames = gene.df$chr,
 			  ranges = IRanges(start = gene.df$start, end = gene.df$end),
 			  strand = gene.df$strand
 			 )
 	names(gene.gr) = gene.df$name
 
 	# Create the feature gr
-	feature.gr = GRanges(seqnames = feature_chr,
+	feature.gr = GenomicRanges::GRanges(seqnames = feature_chr,
 			     ranges = IRanges(start = feature_start, end = feature_end)
 			    )
 
-	# Find the overlaps
-	overlaps.df = as.data.frame(as.matrix(findOverlaps(feature.gr, gene.gr)))
+  # return(list(a = feature.gr, b = gene.gr))
 
-	if(any(nrow(overlaps.df))){
+	# Find the overlaps
+  overlaps.hits = GenomicRanges::findOverlaps(feature.gr, gene.gr)
+
+	if(any(subjectHits(overlaps.hits))){
+
+    # Get the subjectHits
+    subject_hits.v = subjectHits(overlaps.hits)
 
 		# Enter in the genes
-		for(i in 1:nrow(overlaps.df)){
-			plot_gene(gene.df[overlaps.df$subjectHits[i],], y_low, y_high,
-				  feature_start, feature_end, cex_title, geneName, x_pos_title)
+		for(i in 1:length(subject_hits.v)){
+			plot_gene(gene.df[subject_hits.v[i],], y_low, y_high,
+				  feature_start, feature_end, cex_title, geneName, x_pos_title, fwd_gene_col, rev_gene_col)
 		}
 
 	}
@@ -248,7 +208,9 @@ make_gene_schematic = function(feature_chr, feature_start, feature_end,
 }
 
 # Plot gene
-plot_gene = function(gene.v, ylow, yhigh, xstart, xend, cex_title, geneName, x_pos_title = 50){
+plot_gene = function(gene.v, ylow, yhigh, xstart, xend, cex_title, geneName, x_pos_title = 50,
+                     fwd_gene_color, rev_gene_color
+                     ){
 
 	# Get y_mid
 	ymid = (yhigh + ylow) / 2
@@ -257,7 +219,7 @@ plot_gene = function(gene.v, ylow, yhigh, xstart, xend, cex_title, geneName, x_p
 	if(gene.v$strand == "+"){
 
 		# Make the rectangle
-		rect(gene.v$start, ymid + 0.1, gene.v$end, yhigh - 0.1, col = "gray")
+		rect(gene.v$start, ymid + 0.1, gene.v$end, yhigh - 0.1, col = fwd_gene_color)
 
 		if(geneName){
 			if(gene.v$start >= xstart){
@@ -271,7 +233,7 @@ plot_gene = function(gene.v, ylow, yhigh, xstart, xend, cex_title, geneName, x_p
 	}else{
 
 		# Make the rectangle
-		rect(gene.v$start, ylow + 0.1, gene.v$end, ymid - 0.1, col = "gray")
+		rect(gene.v$start, ylow + 0.1, gene.v$end, ymid - 0.1, col = rev_gene_color)
 
 		if(geneName){
 			if(gene.v$end <= xend){
@@ -347,21 +309,6 @@ make_typhoon_plot_axes = function(x_start, x_end, x_step = 500, x_divide = 1000,
 
 
 
-get_mnase_feature_density = function(bam_file_name, chr, start, end, fragL, fragH, bw){
-
-	# Set the jar_file
-	jar_file = "/data/illumina_pipeline/scripts/java_scripts/jar_files/get-mnase-density-R.jar"
-
-	# Create the output string
-	output_str = paste("java -jar", jar_file, bam_file_name, chr, start, end, fragL, fragH, bw)
-
-	# Run the command
-	mnase_dens.v = as.numeric(system(command = output_str, intern = T))
-
-	# Return the mnase_dens.v
-	return(mnase_dens.v)
-
-}
 
 
 
@@ -389,87 +336,39 @@ get_mnase_feature_density = function(bam_file_name, chr, start, end, fragL, frag
 
 
 
-# Create the output typhoon plot
-create_output_plot = function(plot_file_name, typhoon1.m, typhoon2.m,
-			      typhoon1_title, typhoon2_title,
-			      chr, start_coord, end_coord,
-			      plot_width = 10, plot_height = 8, z_max = 5
-			     ){
 
 
-	# Open the plotting window
-	png(file = plot_file_name, width = plot_width, height = plot_height, units = "in", res = 300)
 
-	# Set up the screen
-	close.screen(all.screens = T)
+MergeCoveragePlot = function(g1.m, g2.m, z_max = 10, plot_bty = "o"){
 
-	# Each "screen" is like a mini plot
-	# The syntax is left_coordinate,right_coordinate,bottom_coordinate,top_coordinate, and
-	# each coordinate is scaled from 0 to 1
-	scr.m = matrix(c(0.05, 1, 0.9, 1,
-			 0.05, 1, 0.5, 0.9,
-			 0.05, 1, 0.1, 0.5,
-			 0.05, 1, 0, 0.1,
-			 0, 0.05, 0.1, 0.9
-			), ncol = 4, byrow = T
-		      )
+  # Get the flank
+  flank = (ncol(g1.m) - 1)/2
 
-	# Split the screen
-	# Each "screen" is given an index corresponding to the row in the scr.m matrix
-	scr.s = split.screen(scr.m)
+  # Get the frag_high
+  frag_high = nrow(g1.m)
 
-	# Set the left and right par
-	left_par = 2
-	right_par = 2
+  g1.v = as.vector(t(g1.m))
+  g2.v = as.vector(t(g2.m))
 
-	# Make the gene schematic
-	screen(scr.s[1])
-	par(mar = c(0, left_par, 0, right_par))
+  g1.v[which(g1.v > z_max)] = z_max
+  g2.v[which(g2.v > z_max)] = z_max
+  g1.v[which(g1.v < 0)] = 0
+  g2.v[which(g2.v < 0)] = 0
 
-	# The "set_chromatin_schematic() function just creates a blank plot
-	set_chromatin_schematic()
+  # Create the coordinates for the graph
+  x_left = rep((-flank:flank) - 0.5, frag_high)
+  y_low = rep((1:frag_high) - 0.5, each = (2 * flank + 1))
 
-	# The make_gene_schematic function pulls the genes from the following file:
-	# /home/jab112/feature_files/yeast/gene_tables/sacCer2_ucsc_sgdGeneTable.csv
-	make_gene_schematic(chr, start_coord, end_coord, y_low = 0, y_high = 1, proteinCoding = F)
+  # Create the plot
+  plot(0, 0, type = "n", bty = plot_bty,
+       xlim = c(-flank, flank), xaxs = "i", xaxt = "n",
+       ylim = c(0.5, frag_high + 0.5), yaxs = "i", yaxt = "n",
+       ann = F
+  )
 
-	# Make the first typhoon plot
-	screen(scr.s[2])
-	par(mar = c(3, left_par, 3, right_par))
-	dens_dot_plot(typhoon1.m, z_max = z_max, x_label = "", y_axt = "n", x_axt = "n", plot_title = typhoon1_title)
-	abline(v = 465598, col = "red", lty = 2)
-
-	# Enter in the axes
-	make_typhoon_plot_axes(start_coord, end_coord)
-
-	# Make the second typhoon plot
-	screen(scr.s[3])
-	par(mar = c(3, left_par, 3, right_par))
-	dens_dot_plot(typhoon2.m, z_max = z_max, x_label = "", y_axt = "n", x_axt = "n", plot_title = typhoon2_title)
-	abline(v = 465598, col = "red", lty = 2)
-
-	# Enter in the axes
-	make_typhoon_plot_axes(start_coord, end_coord)
-
-	# Make the x-label
-	screen(scr.s[4])
-	par(mar = c(0, left_par, 0, right_par))
-	set_chromatin_schematic()
-
-	text(x = 0.5, y = 0.5, labels = paste("Chr ", chr, " (kb)", sep = ""), cex = 1.25)
-
-
-	# Make the y-label
-	screen(scr.s[5])
-	par(mar = rep(0, 4))
-	set_chromatin_schematic()
-
-	text(x = 0.5, y = 0.5, labels = "Fragment Length (bp)", srt = 90, cex = 1.25)
-
-	# Close the screen
-	close.screen(all.screens = T)
-
-	# Close the device
-	dev.off()
+  # Add in the colors
+  rect(x_left, y_low, x_left + 1, y_low + 1,
+       col = rgb(g1.v, g2.v, 0, z_max, maxColorValue = z_max), border = NA
+      )
 
 }
---
+++ R/process_bam_functions.R
@@ -0,0 +1,53 @@
+# Function to convert a BAM file to a GR file
+# By default reads in all the reads for the entire chromosome
+# 	Otherwise, specify a particular start_pos and end_pos for just a specific region
+ConvertPairedReadBAMToGR = function(bam_file_name, chr, start_pos = 1, end_pos = -1){
+
+	# Create the BAM File object
+	bf = Rsamtools::BamFile(bam_file_name, index = paste(bam_file_name, ".bai", sep = ""))
+
+	# Get the chr list
+	chr_length.v = Rsamtools::scanBamHeader(bf)$targets
+
+	# Update the end_pos if necessary
+	if(end_pos == -1){
+
+		# Update the end_pos
+		end_pos = chr_length.v[chr]
+
+	}
+
+	# Make a GR file for the chromosome
+	chr.gr = GenomicRanges::GRanges(seqnames = chr,
+			 ranges = IRanges(start = max(start_pos - 250, 1), end = end_pos),
+			 strand = "*"
+			)
+
+	# Specify the scan bam paramaeters
+	p = Rsamtools::ScanBamParam(what = c("pos", "isize"),
+                              which = chr.gr,
+                              flag = Rsamtools::scanBamFlag(isMinusStrand = FALSE)
+                             )
+
+	# Get the reads that meet these conditions
+	reads.l = Rsamtools::scanBam(bf, param = p)
+
+	if(length(reads.l[[1]][["pos"]]) > 0){
+
+		# Convert these reads to a GR object
+		IP.gr = GenomicRanges::GRanges(seqnames = factor(chr, levels = names(chr_length.v)),
+				                           ranges = IRanges::IRanges(start = reads.l[[1]][["pos"]],
+                                                            width = reads.l[[1]][["isize"]]
+                                                   ),
+				                           strand = "*"
+			                            )
+		GenomeInfoDb::seqlengths(IP.gr) = chr_length.v
+
+	}else{
+
+		IP.gr = GenomicRanges::GRanges()
+
+	}
+
+	return(IP.gr)
+}
